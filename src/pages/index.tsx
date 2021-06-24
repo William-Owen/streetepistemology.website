e29755ae96d679f5a3fe2017dd52f558d19b9d40
William Owen <william@wo.dev>
2021-06-24 17:48:17 +0100

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback, useImperativeHandle, useRef } from 'react'
 import clsx from 'clsx'
 import { Link } from 'gatsby'
 import imageEarth from '../images/earth.webp'
-import imageSpace from '../images/space.webp'
 import * as style from './index.module.sass'
 import SEO from '../components/seo.js'
 
 const IndexPage = () => {
+
+	const messages = [
+		[
+			'We landed on the moon in 1969',
+			'The moon landings were a NASA hoax'
+		],
+		[
+			'The earth is flat',
+			'The earth is a globe'
+		],
+		[
+			'Global warming is real',
+			`Global warming isn't real, its a capitalist host!`
+		]
+
+	]
+
 	const [messagesClassName, setMessagesClassName] = useState(
 		style.creativeContainer
 	)
@@ -61,12 +77,19 @@ const IndexPage = () => {
 					className={messagesClassName}
 				>
 					<div>
-						<p className={style.chatPersonA}>
-							We landed on the moon in 1969
-						</p>
-						<p className={style.chatPersonB}>
-							The moon landings were a NASA hoax
-						</p>
+						{
+							messages.map((message, x)=>(
+								<span key={`${x}-1`}>
+									<p className={style.chatPersonA}>
+										{message[0]}
+									</p>
+									<p className={style.chatPersonB}>
+										{message[1]}
+									</p>
+								</span>
+							))
+						}
+
 					</div>
 				</div>
 			</div>
