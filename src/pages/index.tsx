e0be7afd4f0c0dc2af69315c41b702905590fc46
William Owen <william@william-owen.co.uk>
2021-06-25 11:55:00 +0100

    tighter wording

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,60 @@
-import React, { useState, useEffect } from 'react'
-import clsx from 'clsx'
-import { Link } from 'gatsby'
-import imageEarth from '../images/earth.webp'
-import * as style from './index.module.sass'
-import SEO from '../components/seo.js'
+import React, { useState, useEffect } from 'react';
+import clsx from 'clsx';
+import { Link } from 'gatsby';
+import imageEarth from '../images/earth.webp';
+import * as style from './index.module.sass';
+import SEO from '../components/seo.js';
 
 const IndexPage = () => {
 	const messages = [
-		['The earth is flat.', 'The earth is a globe.'],
-		[
-			'We landed on the moon in 1969.',
-			'The moon landings were a NASA hoax.',
-		],
-		[
-			'Global warming is a real concern.',
-			`Global warming isn't real, its a capitalist hoax!`,
-		],
-		['The Earth is 6000 years old.', `The Earth is 4.5 billion years old!`],
-	]
+		[ 'The earth is flat.', 'The earth is a globe.' ],
+		[ 'We landed on the moon in 1969.', 'The moon landings are a NASA hoax.' ],
+		[ 'Global warming is a real concern.', `Global warming is a capitalist hoax!` ],
+		[ 'The Earth is 6000 years old.', `The Earth is 4.5 billion years old!` ]
+	];
 
-	const [messagesClassName, setMessagesClassName] = useState(
-		style.creativeContainer
-	)
-	const rootClassName = clsx([style.pageHomepage, 'pageHomepage'])
+	const [ messagesClassName, setMessagesClassName ] = useState(style.creativeContainer);
+	const rootClassName = clsx([ style.pageHomepage, 'pageHomepage' ]);
 	const doMessages = () => {
-		setMessagesClassName(clsx([style.creativeContainer, style.show]))
-	}
+		setMessagesClassName(clsx([ style.creativeContainer, style.show ]));
+	};
 
 	useEffect(() => {
-		doMessages()
-	}, [])
+		doMessages();
+	}, []);
 
 	return (
 		<article className={rootClassName}>
-			<SEO title='Home' />
+			<SEO title="Home" />
 
 			<div className={style.homepageBanner}>
 				<div className={style.heroMessage}>
 					<div>
-						<h2 className='displayHeading4'>
-							Helping others reflect on their ideas
-						</h2>
+						<h2 className="displayHeading4">Helping others reflect on their ideas</h2>
 						<p>
-							Ideas shape our world, but sometimes talking about
-							them can be difficult. Street Epistemology is a
-							productive non-confrontational way to help others
-							explore and reflect on their ideas and beliefs. Free
-							and open to everyone, learn the tools and start
-							having great conversations.
+							Ideas shape our world, but sometimes talking about them can be difficult. Street
+							Epistemology is a productive non-confrontational way to help others explore and reflect on
+							their ideas and beliefs. Free and open to everyone, learn the tools and start having great
+							conversations.
 						</p>
 						<div className={style.cta}>
-							<Link
-								className={style.btnStartLearning}
-								to='/learn'
-							>
+							<Link className={style.btnStartLearning} to="/learn">
 								Start learning
 							</Link>
-							<Link to='/faqs'>Find out more</Link>
+							<Link to="/faqs">Find out more</Link>
 						</div>
 					</div>
 				</div>
-				<div
-					style={{ backgroundImage: `url(${imageEarth})` }}
-					className={messagesClassName}
-				>
+				<div style={{ backgroundImage: `url(${imageEarth})` }} className={messagesClassName}>
 					<div>
 						{messages.map((message, x) => (
 							<span key={`${x}-1`}>
-								<p
-									style={{ transitionDelay: `${x * 1}s` }}
-									className={style.chatPersonA}
-								>
+								<p style={{ transitionDelay: `${x * 1}s` }} className={style.chatPersonA}>
 									{message[0]}
 								</p>
 								<p
 									style={{
-										transitionDelay: `${x * 1 + 0.5}s`,
+										transitionDelay: `${x * 1 + 0.5}s`
 									}}
 									className={style.chatPersonB}
 								>
@@ -87,7 +66,7 @@ const IndexPage = () => {
 				</div>
 			</div>
 		</article>
-	)
-}
+	);
+};
 
-export default IndexPage
+export default IndexPage;
