11008535cd95f463ab54fdb80173e57574f001fa
William Owen <william@wo.dev>
2021-06-27 14:22:37 +0100

    Added podcasts and FAQ tweeks

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -86,6 +86,7 @@ const Layout = ({ children }) => {
 							<Link to="/resources/communities">Communities</Link>
 							<Link to="/resources/websites">Websites</Link>
 							<Link to="/resources/books">Books</Link>
+							<Link to="/resources/podcasts">Podcasts</Link>
 							<Link to="/resources/support">Support groups</Link>
 						</nav>
 					</div>
