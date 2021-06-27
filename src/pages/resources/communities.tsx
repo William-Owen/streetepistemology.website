347004a0ff8042f3833993247dbc2aa52e07013f
William Owen <william@wo.dev>
2021-06-27 16:20:34 +0100

    Better communities page

--- src/pages/resources/communities.tsx
+++ src/pages/resources/communities.tsx
@@ -1,26 +1,55 @@
 import React from 'react'
 import Page from '../../components/Page'
 import PageHeader from '../../components/PageHeader'
+import PageNavigation from '../../components/PageNavigation'
+import * as style from "./communities.module.sass"
 
 const IndexPage = () => {
 	return (
 		<>
-			<Page>
+			<Page className={style.communitiesPage}>
 
 				<PageHeader sectionHeading='Street Epistemology' heading='Communities' />
 
-				<ul>
+				<article>
 
-					<li><a href="https://discord.gg/sKap3zM">https://discord.gg/sKap3zM</a></li>
-					<li><a href="https://www.facebook.com/StreetEpistemology">https://www.facebook.com/StreetEpistemology</a></li>
-					<li><a href="https://www.facebook.com/groups/learnstreetepistemology/">https://www.facebook.com/groups/learnstreetepistemology/</a></li>
-					<li><a href="https://www.facebook.com/groups/ChatWithAStreetEpistemologist/">https://www.facebook.com/groups/ChatWithAStreetEpistemologist/</a></li>
-					<li><a href="https://www.facebook.com/groups/CritiqueStreetEpistemology/">https://www.facebook.com/groups/CritiqueStreetEpistemology/</a></li>
-					<li><a href="https://www.youtube.com/user/StreetEpistemologist/channels">https://www.youtube.com/user/StreetEpistemologist/channels</a></li>
-					<li><a href="https://twitter.com/StEpistemology">https://twitter.com/StEpistemology</a></li>
-					<li><a href="https://www.reddit.com/r/StreetEpistemology/">https://www.reddit.com/r/StreetEpistemology/</a></li>
+					<main>
 
-				</ul>
+						<h3>About our communities</h3>
+
+						<p>One of the fantastic things about the Street Epistemology community is that it attracts a wide range of people from a diverse collection of background, cultures and philosophical perspectives. By adopting the principles of Street Epistemology, these communities allow for engaging, lively and productive conversations.</p>
+						<p>The communities listed here are run independently by members of the global Street Epistemology community and are not affiliated with any organizations or specific individuals.</p>
+						<p>The following guidelines are a suggestion for ensuring that everyone has a change to engage with the community in a productive and friendly way.</p>
+
+						<ol>
+							<li>Treat everyone and their views charitably and with kindness, especially if you don't like or agree with them.</li>
+							<li>Assume good faith and good intentions, especially when people are perhaps expressing ideas for the first time.</li>
+							<li>Remember, any interaction with someone might be seen, read or overheard by someone new to the community.</li>
+							<li>If you find yourself getting angry, frustrated or irritated we strongly recommend that you take a break.</li>
+							<li>No spam, continuous ranting, excessive self-promotion, provoking conflict, or overuse of mentions.</li>
+							<li>No NSFW content, unless there are clearly designated areas for such content.</li>
+							<li>Do not share any chat logs or recordings from within the community without explicit consent of everyone involved. Remember some people may be exploring positions that might put them in conflict with other communities.</li>
+							<li>Follow the individual platforms terms of service and community guidelines.</li>
+						</ol>
+
+					</main>
+
+					<PageNavigation className={style.navigation}>
+
+						<h3>Online Communities</h3>
+
+						<a href="https://discord.gg/sKap3zM">Street Epistemology on Discord</a>
+						<a href="https://www.facebook.com/StreetEpistemology">Facebook: Street Epistemology</a>
+						<a href="https://www.facebook.com/groups/learnstreetepistemology/">Facebook: Learn Street Epistemology</a>
+						<a href="https://www.facebook.com/groups/ChatWithAStreetEpistemologist/">Facebook: Chat With A Street Epistemologist</a>
+						<a href="https://www.facebook.com/groups/CritiqueStreetEpistemology/">Facebook: Critique Street Epistemology</a>
+						<a href="https://www.youtube.com/user/StreetEpistemologist/channels">YouTube: Street Epistemologist </a>
+						<a href="https://twitter.com/StEpistemology">Twitter: StEpistemology</a>
+						<a href="https://www.reddit.com/r/StreetEpistemology/">Reddit /r/StreetEpistemology</a>
+
+					</PageNavigation>
+
+				</article>
 
 			</Page>
 		</>
